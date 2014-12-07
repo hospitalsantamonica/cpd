bcace0a9fe797e16fece02a42cf0b2724ced6e9e
abrantesasf <abrantesasf@gmail.com>
2014-12-07 12:13:25 -0200

    Correção do SELECT das unidades básicas. Inclusão da sigla no SELECT das unidades auxiliares.

--- mv2000/scripts_sql/mges/unidades.sql
+++ mv2000/scripts_sql/mges/unidades.sql
@@ -11,36 +11,28 @@
 --------------------
 DESC unidade;
 
-SELECT cd_unidade "Cód.",
-       ds_unidade "SELECT u.cd_unidade AS "Cód. Unid. Básicas",
-       u.ds_unidade AS "Unid. Básicas",
-       u.vl_fator AS "Fator Un. Básicas",
-       it.cd_itunidade as "Cód Unid. Auxiliares",
-       it.ds_itunidade AS "Unid. Auxiliares",
-       it.vl_fator AS "Fator Unid. Auxiliares"
-FROM unidade u
-INNER JOIN itunidade it ON (it.cd_unidade = u.cd_unidade)
-WHERE it.ds_itunidade NOT LIKE '%INATIVO%'
-ORDER BY u.ds_unidade, it.ds_itunidade;
-Descrição",
+SELECT cd_unidade AS "Cód. Unid. Básicas",
+       ds_unidade AS "Descrição",
        vl_fator "Fator"
 FROM unidade
-ORDER BY cd_unidade;
+WHERE ds_unidade NOT LIKE '%INATIVO%'
+ORDER BY ds_unidade;
 
 
 
 
--- UNIDADES AUXILIARES:
------------------------
+-- UNIDADES BÁSICAS E AUXILIARES:
+---------------------------------
 DESC itunidade;
 
 SELECT u.cd_unidade AS "Cód. Unid. Básicas",
        u.ds_unidade AS "Unid. Básicas",
        u.vl_fator AS "Fator Un. Básicas",
-       it.cd_itunidade as "Cód Unid. Auxiliares",
+       it.cd_itunidade AS "Cód Unid. Auxiliares",
+       it.nm_itunidade AS "Sigla Unid. Aux.",
        it.ds_itunidade AS "Unid. Auxiliares",
        it.vl_fator AS "Fator Unid. Auxiliares"
 FROM unidade u
 INNER JOIN itunidade it ON (it.cd_unidade = u.cd_unidade)
---WHERE it.ds_itunidade NOT LIKE '%INATIVO%'
+WHERE it.ds_itunidade NOT LIKE '%INATIVO%'
 ORDER BY u.ds_unidade, it.ds_itunidade;

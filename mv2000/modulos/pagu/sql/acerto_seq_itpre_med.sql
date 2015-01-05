4304dca9b13d38424758db875be0240fde2b29b8
phelipemda <phelipe.mendonca@gmail.com>
2015-01-05 21:00:36 -0200

    acerto da seq correta
    configurado para a sequence correta

--- mv2000/modulos/pagu/sql/acerto_seq_itpre_med.sql
+++ mv2000/modulos/pagu/sql/acerto_seq_itpre_med.sql
@@ -1,14 +1,14 @@
--- ACERTO DA SEQUENCE PARA NOVOS ITENS DE PRESCRIÃ‡ÃƒO
+-- ACERTO DA SEQUENCE PARA NOVOS ITENS DE PRESCRIÇÃO
 -- =================================================
 -- Este script aumenta o contador da sequence de itens
--- de prescriÃ§Ã£o do MV para permitir que os novos cadastros
--- se iniciem a partir do nÃºmero 5.000
--- Por: Abrantes AraÃºjo Silva Filho
+-- de prescrição do MV para permitir que os novos cadastros
+-- se iniciem a partir do número 5.000
+-- Por: Abrantes Araújo Silva Filho
 --      abrantesasf@gmail.com
 -- Em: 2014-12-16
 
 -- ============================================================ --
--- PELO AMOR DE DEUS, NÃƒO RODE ESTE SCRIPT NO BANCO DE DADOS!!! --
+-- PELO AMOR DE DEUS, NÃO RODE ESTE SCRIPT NO BANCO DE DADOS!!! --
 -- ============================================================ --
 
 
@@ -16,24 +16,27 @@ DECLARE
   v_contador NUMBER := NULL;
 
 BEGIN
-  -- Pega prÃ³ximo nÃºmero da sequence atual
-  SELECT seq_itpre_med.nextval INTO v_contador
+  -- Pega próximo número da sequence atual
+  SELECT seq_tip_presc.nextval INTO v_contador
   FROM dual;
   
-  -- Loop para aumentar a sequence. A lÃ³gica Ã© a seguinte: por exemplo, se vocÃª quiser
-  -- que os novos cadastros comecem com o cÃ³digo 25000, vocÃª deverÃ¡ colocar a condiÃ§ao
+  -- Loop para aumentar a sequence. A lógica é a seguinte: por exemplo, se você quiser
+  -- que os novos cadastros comecem com o código 25000, você deverá colocar a condiçao
   -- no contador para "< 24999". Isso porque quando o contador chegar a 24998, o loop
-  -- serÃ¡ executado mais uma vez e o valor da sequence serÃ¡ aumentado para 24999. AÃ­
-  -- o prÃ³ximo cadastro ja comeÃ§arÃ¡ com o cÃ³digo 25000.
-  -- Se a condiÃ§Ã£o do while fosse "< 25000", quando o contador chegasse em 24999
+  -- será executado mais uma vez e o valor da sequence será aumentado para 24999. Aí
+  -- o próximo cadastro ja começará com o código 25000.
+  -- Se a condição do while fosse "< 25000", quando o contador chegasse em 24999
   -- o loop seria executado mais uma vez e a sequence seria aumentada para 25000 (e
-  -- o prÃ³ximo cadastro teria o cÃ³digo 25001, e nÃ£o 25000 como desejado).
+  -- o próximo cadastro teria o código 25001, e não 25000 como desejado).
   --
-  -- RESUMINDO: o valor que vocÃª deve colocar na condiÃ§Ã£o do while Ã©:
+  -- RESUMINDO: o valor que você deve colocar na condição do while é:
   --            "sequence desejada" - 1
   WHILE v_contador < 4999 LOOP
-    SELECT seq_itpre_med.nextval INTO v_contador
+    SELECT seq_tip_presc.nextval INTO v_contador
     FROM dual;
   END LOOP;
 
 END;
+
+SELECT seq_tip_presc.currval 
+    FROM dual;
